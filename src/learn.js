a561983f3765651260b0d1b59c0980e2077bc07e
mjmohona <mj.mohonajahan@gmail.com>
2024-11-14 15:04:02 +0600

    latest update

--- src/learn.js
+++ src/learn.js
@@ -11,14 +11,26 @@
 // console.log(result);
 
 // What if want to copy array and divide by 2?
-function copyArrayAndDevideBy2(array) {
+// function copyArrayAndDevideBy2(array) {
+//   const output = [];
+//   for(let i= 0; i< array.length; i++){
+//     output.push(array[i]/2);
+//   }
+//   return output;
+// }
+// const myArray = [1,2,3];
+// const result = copyArrayAndDevideBy2(myArray);
+
+function copyArrayAndManupulate(array, instructions) {
   const output = [];
   for(let i= 0; i< array.length; i++){
-    output.push(array[i]/2);
+    output.push(instructions(array[i]));
   }
   return output;
 }
-const myArray = [1,2,3];
-const result = copyArrayAndDevideBy2(myArray);
+function multiplyBy2(input) {
+  return input * 2;
+}
+const result = copyArrayAndManupulate([1,2,3], multiplyBy2);
 
 console.log(result);
